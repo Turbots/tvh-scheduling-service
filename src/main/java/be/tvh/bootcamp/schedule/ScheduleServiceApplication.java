89039993e0287ed9318dca1e08f91169024dc935
Dieter Hubau <dhubau@gmail.com>
2017-10-19 17:00:44 +0200

    I changed something

--- src/main/java/be/tvh/bootcamp/schedule/ScheduleServiceApplication.java
+++ src/main/java/be/tvh/bootcamp/schedule/ScheduleServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ScheduleServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("CHANGE!");
 		SpringApplication.run(ScheduleServiceApplication.class, args);
 	}
 }
